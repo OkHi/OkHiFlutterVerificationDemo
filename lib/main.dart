43300b5d2b2b1a67bc32f4429b4a7a912a5e7d07
Julius Kiano <jgkiano@gmail.com>
2024-12-12 13:55:14 +0300

    added demo code

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:okhi_flutter/models/okhi_usage_type.dart';
+import 'package:okhi_flutter/okhi_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _launch = false;
+
+  @override
+  void initState() {
+    super.initState();
+    final config = OkHiAppConfiguration(
+      branchId: "", // your branch ID
+      clientKey: "", // your client key
+      env: OkHiEnv.sandbox,
+      notification: OkHiAndroidNotification(
+        title: "Verification in progress",
+        text: "Verifying your address",
+        channelId: "okhi",
+        channelName: "OkHi",
+        channelDescription: "Verification alerts",
+      ),
+    );
+    OkHi.initialize(config);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('OkHi Flutter Demo'),
         ),
+        body: _renderBody(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  _renderBody() {
+    if (!_launch) {
+      return Center(
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _launch = true;
+            });
+          },
+          child: const Text('Verify an address'),
+        ),
+      );
+    }
+    return OkHiLocationManager(
+      user: _createOkHiUser(),
+      onCloseRequest: _handleOnClose,
+      onError: _handleOnError,
+      onSucess: _handleOnSuccess,
+      configuration: OkHiLocationManagerConfiguration(
+        usageTypes: [UsageType.digitalVerification],
+      ),
+    );
+  }
+
+  OkHiUser _createOkHiUser() {
+    return OkHiUser(
+      phone: "+2547..",
+      firstName: "John",
+      lastName: "Doe",
+      appUserId: "abcd1234",
+      email: "john@okhi.co",
+    );
+  }
+
+  _handleOnSuccess(OkHiLocationManagerResponse response) async {
+    setState(() {
+      _launch = false;
+    });
+    final String locationId = await response.startVerification(null);
+    print("started verification for $locationId");
+  }
+
+  _handleOnError(OkHiException exception) {
+    print(exception.code);
+    print(exception.message);
+  }
+
+  _handleOnClose() {
+    setState(() {
+      _launch = false;
+    });
   }
 }
