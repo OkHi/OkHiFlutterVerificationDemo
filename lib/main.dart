23d3df99608a85f575b5f641dcdffb8c05a37715
Julius Kiano <jgkiano@gmail.com>
2024-12-12 13:56:59 +0300

    update to physicalVerification

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class _MyAppState extends State<MyApp> {
       onError: _handleOnError,
       onSucess: _handleOnSuccess,
       configuration: OkHiLocationManagerConfiguration(
-        usageTypes: [UsageType.digitalVerification],
+        usageTypes: [UsageType.physicalVerification],
       ),
     );
   }
